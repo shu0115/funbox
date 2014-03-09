180a666ef0729f103c9e6bb15951a03d8822a02f
shu0115 <s.matsumoto0115@gmail.com>
2014-03-09 16:44:36 +0900

    不要行削除

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -18,37 +18,7 @@ class PlaylistsController < ApplicationController
     unless @playlist.tracks.exists?
       @videos = Track.youtube_search(@word, @unique_ids, page)
     end
-
-    # if word.present?
-    #   # YouTube検索
-    #   videos = YouTubeIt::Client.new.videos_by(query: word, order_by: 'viewCount', max_results: 50).videos
-    #   # プレイリスト登録済み動画除外
-    #   videos.delete_if{ |v| @unique_ids.index(v.unique_id) }
-    #   @videos = Kaminari.paginate_array(videos).page(page).per(Settings.per_page)
-    # else
-    #   @videos = []
-    # end
   end
-
-  # # 動画追加
-  # def video(id, word, video)
-  #   playlist = Playlist.find_by(id: id, user_id: current_user.id)
-  #   videos = YouTubeIt::Client.new.videos_by(query: word, page: 1, per_page: 3).videos
-
-  #   track = Track.where(user_id: current_user.id, playlist_id: playlist.id, unique_id: video['unique_id'], provider: 'youtube').first_or_create(
-  #     author_name: video['author_name'],
-  #     duration: video['duration'],
-  #     description: video['description'],
-  #     title: video['title'],
-  #     unique_id: video['unique_id'],
-  #     published_at: video['published_at'],
-  #     favorite_count: video['favorite_count'],
-  #     view_count: video['view_count'],
-  #     player_url: video['player_url']
-  #   )
-
-  #   render partial: '/playlists/add_button', locals: { track: track, playlist: playlist, word: word, videos: videos }
-  # end
 
   # GET /playlists/new
   def new
@@ -86,8 +56,6 @@ class PlaylistsController < ApplicationController
   # DELETE /playlists/1
   def destroy(id)
     current_user.playlists.find_by(id: id).destroy
-    # @playlist = Playlist.find(id)
-    # @playlist.mine.destroy
 
     redirect_to playlists_url
   end
--- app/controllers/track_controller.rb
+++ app/controllers/track_controller.rb
@@ -4,14 +4,12 @@ class TrackController < ApplicationController
     playlist = Playlist.includes(:tracks).mine(current_user).find_by(id: playlist_id)
     tracks   = playlist.tracks.order(created_at: :asc)
 
-    # render partial: '/playlists/tracks', locals: { playlist: playlist, tracks: tracks }
     render partial: '/playlists/play_all_area', locals: { playlist: playlist, tracks: tracks }
   end
 
   # 動画追加
   def create(playlist_id, word, video, page)
     playlist = Playlist.find_by(id: playlist_id, user_id: current_user.id)
-    # videos = YouTubeIt::Client.new.videos_by(query: word, order_by: 'viewCount', page: page, per_page: Settings.per_page).videos
 
     track = Track.where(user_id: current_user.id, playlist_id: playlist.id, unique_id: video['unique_id'], provider: 'youtube').first_or_create(
       author_name: video['author_name'],
