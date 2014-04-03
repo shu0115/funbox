92a3ef1ef3a3db144d5ac71bd675eecbdb6c51df
shu0115 <s.matsumoto0115@gmail.com>
2014-04-03 11:15:18 +0900

    ユーザプレイリストリンク修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   skip_before_filter :authenticate
 
-  # ユーザ一覧
-  def index(page)
-    @users = User.order(created_at: :desc).page(page).per(50)
-  end
+  # # ユーザ一覧
+  # def index(page)
+  #   @users = User.order(created_at: :desc).page(page).per(50)
+  # end
 
   # ユーザのプレイリスト一覧
   def playlists(user_id)
@@ -12,15 +12,15 @@ class UsersController < ApplicationController
     @playlists = Playlist.where(user_id: user_id).order(created_at: :desc)
   end
 
-  # プレイリスト内トラック再生
-  def playlist(user_id, id)
-    session[:request_url] = user_playlist_url(user_id, id) if current_user.blank?
+  # # プレイリスト内トラック再生
+  # def playlist(user_id, id)
+  #   session[:request_url] = user_playlist_url(user_id, id) if current_user.blank?
 
-    @user       = User.find_by(id: user_id)
-    @playlist   = Playlist.includes(:tracks).order("tracks.created_at ASC").find(id)
-    @tracks     = @playlist.tracks
-    @unique_ids = @tracks.pluck(:unique_id)
-  end
+  #   @user       = User.find_by(id: user_id)
+  #   @playlist   = Playlist.includes(:tracks).order("tracks.created_at ASC").find(id)
+  #   @tracks     = @playlist.tracks
+  #   @unique_ids = @tracks.pluck(:unique_id)
+  # end
 
   # ユーザのトラック全体から100曲ランダム再生
   def playall(user_id)
@@ -30,16 +30,16 @@ class UsersController < ApplicationController
     @tracks_hash = @tracks.index_by{ |x| x.unique_id }
   end
 
-  # プレイリストに対するGood
-  def playlist_good_toggle(id)
-    playlist = Playlist.find_by(id: id)
-    good = Good.where(user_id: current_user.id, playlist_id: playlist.id).first_or_initialize
-    if good.id.present?
-      good.destroy
-    else
-      good.save!
-    end
+  # # プレイリストに対するGood
+  # def playlist_good_toggle(id)
+  #   playlist = Playlist.find_by(id: id)
+  #   good = Good.where(user_id: current_user.id, playlist_id: playlist.id).first_or_initialize
+  #   if good.id.present?
+  #     good.destroy
+  #   else
+  #     good.save!
+  #   end
 
-    render partial: '/users/playlist_good', locals: { playlist: playlist }
-  end
+  #   render partial: '/users/playlist_good', locals: { playlist: playlist }
+  # end
 end
