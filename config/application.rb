611f374ce1dbbd6e3f43f1769d067b115919de2b
shu0115 <s.matsumoto0115@gmail.com>
2015-05-01 14:04:55 +0900

    tapp設定変更

--- config/application.rb
+++ config/application.rb
@@ -13,9 +13,6 @@ Bundler.require(:default, Rails.env)
 
 module Funbox
   class Application < Rails::Application
-    # For Tapp
-    Tapp.config.default_printer = :awesome_print
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
