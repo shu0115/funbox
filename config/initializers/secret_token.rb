1c8bdfa6d0f2c320d30854b77f36d6f7f18d05a0
shu0115 <s.matsumoto0115@gmail.com>
2017-04-27 19:21:12 +0900

    Update

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Funbox::Application.config.secret_key_base = '798a492b5e5faf90e41bcccc7a1fb4ec48eb2ce61da9d7ded107e05bb4a8a7c04974ed6c796c191e52b8d89531bc4ad1f5de4b54a4328f6a927fd45ba27d95be'
+Funbox::Application.config.secret_key_base = Settings.secret_key_base
